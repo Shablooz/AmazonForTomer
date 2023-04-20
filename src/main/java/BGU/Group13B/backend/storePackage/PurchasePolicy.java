96b4676902c54f70e279b29010ca6b15ae9bc9f5
lior-yishay <lioryi@post.bgu.ac.il>
2023-04-20 13:50:23 +0300

    init maxAmount in PurchasePolicy to 0. #32

--- src/main/java/BGU/Group13B/backend/storePackage/PurchasePolicy.java
+++ src/main/java/BGU/Group13B/backend/storePackage/PurchasePolicy.java
@@ -1,9 +1,9 @@
 package BGU.Group13B.backend.storePackage;
 
 public class PurchasePolicy {
-    private int maxAmount;
+    private int maxAmount = 0;
     public void checkPolicy(int productQuantity) {
-        if (productQuantity >maxAmount)
+        if (productQuantity > maxAmount)
             throw new RuntimeException("Cant buy more than the amount: " + productQuantity);
     }
 }
