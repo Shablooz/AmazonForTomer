6f468de3ccc10e41ef4858ba7842e1be754b7d42
Shaun Shuster <shaunshu@post.bgu.ac.il>
2023-06-02 16:12:01 +0300

    merge

--- src/main/java/BGU/Group13B/backend/storePackage/WorkerCard.java
+++ src/main/java/BGU/Group13B/backend/storePackage/WorkerCard.java
@@ -2,7 +2,6 @@ package BGU.Group13B.backend.storePackage;
 
 import BGU.Group13B.backend.User.UserPermissions;
 
-import java.util.List;
 import java.util.Set;
 
 public record WorkerCard (int userId, UserPermissions.StoreRole storeRole, Set<UserPermissions.IndividualPermission> userPermissions){
