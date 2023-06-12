440f1fa279dcee416ccde4a655b79bdd66580cc2
Tomer Ofek <tomerofe@post.bgu.ac.il>
2023-06-12 21:50:20 +0300

    need to fix mistake in map in market (getAllUserAssociatedStores function)

--- src/main/java/BGU/Group13B/backend/Repositories/Implementations/StoreRepositoryImpl/StoreRepoService.java
+++ src/main/java/BGU/Group13B/backend/Repositories/Implementations/StoreRepositoryImpl/StoreRepoService.java
@@ -22,6 +22,7 @@ public class StoreRepoService {
 
     public void save(StoreRepositoryAsList storeRepositoryAsList){
         SingletonCollection.setStoreRepository(storeRepoJPA.save(storeRepositoryAsList));
+        //storeRepoJPA.save(storeRepositoryAsList);
     }
 
     public void delete(StoreRepositoryAsList storeRepositoryAsList){
