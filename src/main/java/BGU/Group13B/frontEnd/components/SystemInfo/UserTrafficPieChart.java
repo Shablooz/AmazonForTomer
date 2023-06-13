57c9e6d02e286ba98f2065fd853455ed1e817d55
lior-yishay <lioryi@post.bgu.ac.il>
2023-06-13 17:58:11 +0300

    adding the user traffic view with no navigation and without real-time updates

---
+++ src/main/java/BGU/Group13B/frontEnd/components/SystemInfo/UserTrafficPieChart.java
@@ -0,0 +1,80 @@
+package BGU.Group13B.frontEnd.components.SystemInfo;
+
+import BGU.Group13B.backend.System.UserTrafficRecord;
+import BGU.Group13B.frontEnd.components.views.SystemInfoView;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.style.Color;
+import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.DataSeries;
+import com.vaadin.flow.component.charts.model.DataSeriesItem;
+import com.vaadin.flow.component.charts.model.style.SolidColor;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import java.time.LocalDate;
+
+public class UserTrafficPieChart extends VerticalLayout {
+
+    private final Chart chart = new Chart();
+
+    private final DatePicker startDate = new DatePicker("Start Date");
+    private final DatePicker endDate = new DatePicker("End Date");
+
+    private final HorizontalLayout dateLayout = new HorizontalLayout(startDate, endDate);
+
+
+    public UserTrafficPieChart(SystemInfoView systemInfoView, UserTrafficRecord userTrafficRecord, LocalDate start, LocalDate end){
+        this.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        //date range picker
+        startDate.setRequired(true);
+        endDate.setRequired(true);
+        startDate.setRequiredIndicatorVisible(true);
+        endDate.setRequiredIndicatorVisible(true);
+        startDate.setValue(start);
+        endDate.setValue(end);
+
+        startDate.addValueChangeListener(event -> {
+            if(startDate.getValue().isAfter(endDate.getValue()))
+                endDate.setValue(startDate.getValue());
+            systemInfoView.setUserTrafficChartValues(startDate.getValue(), endDate.getValue());
+        });
+
+        endDate.addValueChangeListener(event -> {
+            if(endDate.getValue().isBefore(startDate.getValue()))
+                startDate.setValue(endDate.getValue());
+            systemInfoView.setUserTrafficChartValues(startDate.getValue(), endDate.getValue());
+        });
+
+        chart.getConfiguration().getChart().setType(ChartType.PIE);
+        setData(userTrafficRecord);
+
+        add(dateLayout, chart);
+
+        //style
+        chart.getConfiguration().getTooltip().setPointFormat("{series.name}: <b>{point.percentage:.1f}%</b>");
+        chart.getConfiguration().getChart().setBackgroundColor(new SolidColor(0, 0, 0, 0)); // transparent background
+    }
+
+    public void setData(UserTrafficRecord userTrafficRecord){
+        DataSeries series = new DataSeries();
+        series.add(new DataSeriesItem("Guests", userTrafficRecord.numOfGuests()));
+        series.add(new DataSeriesItem("Members", userTrafficRecord.numOfRegularMembers()));
+        series.add(new DataSeriesItem("Managers", userTrafficRecord.numOfStoreManagersThatAreNotOwners()));
+        series.add(new DataSeriesItem("Owners", userTrafficRecord.numOfStoreOwners()));
+        series.add(new DataSeriesItem("Admins", userTrafficRecord.numOfAdmins()));
+        chart.getConfiguration().setSeries(series);
+        chart.getConfiguration().setTitle("Number Of Visitors: " + userTrafficRecord.getTotalVisitors());
+        chart.drawChart();
+    }
+
+
+    public LocalDate getStartDate() {
+        return startDate.getValue();
+    }
+
+    public LocalDate getEndDate() {
+        return endDate.getValue();
+    }
+}
--- src/main/java/BGU/Group13B/frontEnd/components/views/SystemInfoView.java
+++ src/main/java/BGU/Group13B/frontEnd/components/views/SystemInfoView.java
@@ -1,4 +1,75 @@
 package BGU.Group13B.frontEnd.components.views;
 
-public class SystemInfoView {
+import BGU.Group13B.backend.System.UserTrafficRecord;
+import BGU.Group13B.frontEnd.ResponseHandler;
+import BGU.Group13B.frontEnd.components.SessionToIdMapper;
+import BGU.Group13B.frontEnd.components.SystemInfo.UserTrafficPieChart;
+import BGU.Group13B.service.Session;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+@PageTitle("System Info")
+@Route(value = "systeminfo", layout = MainLayout.class)
+public class SystemInfoView extends VerticalLayout implements ResponseHandler, BeforeEnterObserver {
+
+    private int userId;
+    private final Session session;
+
+    //components
+    private UserTrafficPieChart userTrafficPieChart;
+
+
+    @Autowired
+    public SystemInfoView(Session session){
+        this.session = session;
+    }
+
+    private void start(){
+        removeAll();
+        LocalDate today = LocalDate.now();
+
+        UserTrafficRecord userTraffic = handleResponse(session.getUserTrafficOfRange(userId, today, today));
+        if(userTraffic == null)
+            return;
+
+        userTrafficPieChart = new UserTrafficPieChart(this, userTraffic, today, today);
+
+        add(userTrafficPieChart);
+    }
+
+    public void setUserTrafficChartValues(LocalDate start, LocalDate end){
+        UserTrafficRecord userTraffic = handleResponse(session.getUserTrafficOfRange(userId, start, end));
+        if(userTraffic == null)
+            return;
+
+        userTrafficPieChart.setData(userTraffic);
+    }
+
+    public void refreshUserTrafficChart(){
+        UserTrafficRecord userTraffic = handleResponse(session.getUserTrafficOfRange(userId, userTrafficPieChart.getStartDate(), userTrafficPieChart.getEndDate()));
+        if(userTraffic == null){
+            notifyWarning("Failed to refresh user traffic chart");
+            return;
+        }
+        userTrafficPieChart.setData(userTraffic);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        userId = SessionToIdMapper.getInstance().getCurrentSessionId();
+
+        var isAdmin = handleResponse(session.isAdmin(userId));
+
+        if(isAdmin == null || !isAdmin){
+            beforeEnterEvent.rerouteTo(HomeView.class);
+            return;
+        }
+        start();
+    }
 }
